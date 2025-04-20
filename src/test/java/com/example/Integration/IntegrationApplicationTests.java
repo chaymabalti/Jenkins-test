e4c2c49a0054b445356a3b6a4383eac1d8a8b7de
baltichaima <chaimabalti32@gmail.com>
2025-04-20 22:33:08 +0200

    2eme chg

--- src/test/java/com/example/Integration/IntegrationApplicationTests.java
+++ src/test/java/com/example/Integration/IntegrationApplicationTests.java
@@ -1,5 +1,5 @@
 package com.example.Integration;
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
